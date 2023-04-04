13d028322c4aaec6465b9630b48b74acc22f070d
URIAN VIERA <urian1213viera@gmail.com>
2023-04-03 22:09:29 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -86,18 +86,5 @@ def procesarCheckboxPersona():
         return jsonify({"idsProcesados": idsConsigChecked})
 
 
-
-
-@app.errorhandler(404)
-def not_found(error):
-    return render_template('404.html', error=error)
-
-
-@app.errorhandler(500)
-def server_error(error):
-    return render_template('500.html', error=error)
-
-
-
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
